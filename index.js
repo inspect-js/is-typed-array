da366820a2bcfd1608870a13cdd5af285e7bd27d
Jordan Harband <ljharb@gmail.com>
2015-10-02 01:20:45 -0700

    Implementation

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var forEach = require('foreach');
+
+var toStr = Object.prototype.toString;
+var hasToStringTag = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol';
+
+var typedArrays = {
+	Float32Array: true,
+	Float64Array: true,
+	Int8Array: true,
+	Int16Array: true,
+	Int32Array: true,
+	Uint8Array: true,
+	Uint8ClampedArray: true,
+	Uint16Array: true,
+	Uint32Array: true
+};
+
+var slice = String.prototype.slice;
+var toStrTags = {};
+var gOPD = Object.getOwnPropertyDescriptor;
+if (hasToStringTag && gOPD && Object.getPrototypeOf) {
+	forEach(Object.keys(typedArrays), function (typedArray) {
+		var arr = new global[typedArray]();
+		var proto = Object.getPrototypeOf(arr);
+		toStrTags[typedArray] = gOPD(proto, Symbol.toStringTag).get;
+	});
+}
+
+var tryTypedArrays = function tryTypedArrays(value) {
+	var anyTrue = false;
+	forEach(toStrTags, function (getter, typedArray) {
+		if (!anyTrue) {
+			try {
+				anyTrue = getter.call(value) === typedArray;
+			} catch (e) {/**/}
+		}
+	});
+	return anyTrue;
+};
+
+module.exports = function isTypedArray(value) {
+	if (!value || typeof value !== 'object') { return false; }
+	if (!hasToStringTag) { return !!typedArrays[slice.call(toStr.call(value), 8, -1)]; }
+	if (!gOPD) { return false; }
+	return tryTypedArrays(value);
+};
