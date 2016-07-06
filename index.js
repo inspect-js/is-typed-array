c627b203c40637bb4f47555a3c24a8c34d31340e
xguest <xguest@list.ru>
2016-07-06 07:54:18 +0500

    optimized index.js

--- index.js
+++ index.js
@@ -1,56 +1,8 @@
 'use strict';
 
-var forEach = require('foreach');
-
 var toStr = Object.prototype.toString;
-var hasToStringTag = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol';
 
-var typedArrays = {
-	Float32Array: true,
-	Float64Array: true,
-	Int8Array: true,
-	Int16Array: true,
-	Int32Array: true,
-	Uint8Array: true,
-	Uint8ClampedArray: true,
-	Uint16Array: true,
-	Uint32Array: true
-};
-
-var slice = String.prototype.slice;
-var toStrTags = {};
-var gOPD = Object.getOwnPropertyDescriptor;
-if (hasToStringTag && gOPD && Object.getPrototypeOf) {
-	forEach(typedArrays, function (_, typedArray) {
-		var arr = new global[typedArray]();
-		if (!(Symbol.toStringTag in arr)) {
-			throw new EvalError('this engine has support for Symbol.toStringTag, but ' + typedArray + ' does not have the property! Please report this.');
-		}
-		var proto = Object.getPrototypeOf(arr);
-		var descriptor = gOPD(proto, Symbol.toStringTag);
-		if (!descriptor) {
-			var superProto = Object.getPrototypeOf(proto);
-			descriptor = gOPD(superProto, Symbol.toStringTag);
-		}
-		toStrTags[typedArray] = descriptor.get;
-	});
-}
-
-var tryTypedArrays = function tryTypedArrays(value) {
-	var anyTrue = false;
-	forEach(toStrTags, function (getter, typedArray) {
-		if (!anyTrue) {
-			try {
-				anyTrue = getter.call(value) === typedArray;
-			} catch (e) { /**/ }
-		}
-	});
-	return anyTrue;
-};
-
-module.exports = function isTypedArray(value) {
-	if (!value || typeof value !== 'object') { return false; }
-	if (!hasToStringTag) { return !!typedArrays[slice.call(toStr.call(value), 8, -1)]; }
-	if (!gOPD) { return false; }
-	return tryTypedArrays(value);
+module.exports = function (value) {
+	var name = toStr.call(value).replace(/\[.+?(\w+)\]/, '$1');
+	return name === Object(value).constructor.name && (/.*array.*/img).test(name);
 };
