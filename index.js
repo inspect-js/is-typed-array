95769d2e417f83f0beb5e22f0fe8dadae004e196
xguest <xguest@list.ru>
2016-07-11 00:16:34 +0500

    RegExp fixed

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 'use strict';
 
-var typedArrays = /[Float[32|64]|Uint8Clamped|[Int|Uint][8|16|32]]Array/;
+var typedArrays = function (value) {
+	/* eslint no-return-assign: 1 */
+	var reg = /.*((Float|Uint|Int)(8Clamped|8|16|32|64))Array.*/;
+	return reg.test(value);
+/**
+	// If use 'return (reg = value.replace(reg, "$1")) !== value && reg;'
+	// function will return the name of the data stored in the array, it is true
+	// or false.
+	//	Если использовать 'return (reg = value.replace(reg, "$1")) !== value && reg;'
+	//	функция будет возвращать имя хранимых данных в массиве, это true или false.
+*/
+};
 
 module.exports = Object.getPrototypeOf && Object.getOwnPropertyDescriptor && typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol' ? function (value) {
 	var proto, descriptor;
 	/* eslint no-return-assign: 1 */
-	return !!(Object(value) === value && (
+	return Object(value) === value && (
 		(descriptor = Object.getOwnPropertyDescriptor(
 			(proto = Object.getPrototypeOf(value), Object.getPrototypeOf(proto) || proto),
 			Symbol.toStringTag)
-		) && typedArrays.test(descriptor.get.call(value))
-	));
+		) && typedArrays(descriptor.get.call(value))
+	);
 } : function (value) {
-	return !!(Object(value) === value && typedArrays.test({}.toString.call(value)));
+	return typedArrays({}.toString.call(value));
 };
