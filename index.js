15b74085fd76079990b6b03122773c70b2c7331e
xguest <xguest@list.ru>
2016-07-14 00:04:20 +0500

    Fixed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var has = require('has');
+
 var toStr = Object.prototype.toString;
+var hasToStringTag = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol';
+
 var typedArrays = {
 	Float32Array: true,
 	Float64Array: true,
@@ -13,44 +16,38 @@ var typedArrays = {
 	Uint16Array: true,
 	Uint32Array: true
 };
+
 var slice = String.prototype.slice;
-var getpr = Object.getPrototypeOf;
 var gOPD = Object.getOwnPropertyDescriptor;
-var isTypedArray;
-if (getpr && gOPD) {
-	if (typeof Symbol === 'function') {
-		if (typeof Symbol.toStringTag === 'symbol') {
-			isTypedArray = function (value) {
-				var typedArray, arr, proto, descriptor;
-				/* jscs:disable disallowNodeTypes */
-				for (typedArray in typedArrays) {
-					if (has(typedArrays, typedArray)) {
-						arr = new global[typedArray]();
-						if (!(Symbol.toStringTag in arr)) {
-							throw new EvalError('this engine has support for Symbol.toStringTag, but ' + typedArray + ' does not have the property! Please report this.');
-						}
-						proto = getpr(arr);
-						descriptor = gOPD(proto, Symbol.toStringTag);
-						if (!descriptor) {
-							proto = getpr(proto);
-							descriptor = gOPD(proto, Symbol.toStringTag);
-						}
-						if (descriptor) {
-							descriptor = typedArrays[descriptor.get.call(Object(value))];
-							if (descriptor) { return descriptor; }
-						}
-					}
+var tryTypedArrays = function tryTypedArrays(value) {
+	if (Object.getPrototypeOf) {
+		var typedArray, arr, proto, descriptor;
+		/* jscs:disable disallowNodeTypes */
+		for (typedArray in typedArrays) {
+			if (has(typedArrays, typedArray)) {
+				arr = new global[typedArray]();
+				if (!(Symbol.toStringTag in arr)) {
+					throw new EvalError('this engine has support for Symbol.toStringTag, but ' + typedArray + ' does not have the property! Please report this.');
 				}
-				/* jscs:enable disallowNodeTypes */
-				return false;
-			};
+				proto = Object.getPrototypeOf(arr);
+				descriptor = gOPD(proto, Symbol.toStringTag);
+				if (!descriptor) {
+					proto = Object.getPrototypeOf(proto);
+					descriptor = gOPD(proto, Symbol.toStringTag);
+				}
+				if (descriptor) {
+					return typedArrays[descriptor.get.call(Object(value))];
+				}
+			}
 		}
 	}
-}
-if (!isTypedArray) {
-	isTypedArray = function (value) {
-		return typedArrays[slice.call(toStr.call(Object(value)), 8, -1)] || false;
-	};
-}
+	/* jscs:enable disallowNodeTypes */
+	return false;
+};
 
-module.exports = isTypedArray;
+module.exports = function isTypedArray(value) {
+	if (!value || typeof value !== 'object') { return false; }
+	if (!hasToStringTag) { return !!typedArrays[slice.call(toStr.call(value), 8, -1)]; }
+	if (!gOPD) { return false; }
+	return tryTypedArrays(value);
+};
