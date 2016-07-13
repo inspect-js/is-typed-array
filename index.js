87f0e8709f5f14814ca7ad6271ae148abfd882fc
xguest <xguest@list.ru>
2016-07-13 18:56:28 +0500

    replacing the use of forEach

--- index.js
+++ index.js
@@ -1,56 +1,45 @@
 'use strict';
 
-var forEach = require('foreach');
+var toStr = Object.prototype.toString,
+	typedArrays = {
+		Float32Array: true,
+		Float64Array: true,
+		Int8Array: true,
+		Int16Array: true,
+		Int32Array: true,
+		Uint8Array: true,
+		Uint8ClampedArray: true,
+		Uint16Array: true,
+		Uint32Array: true
+	},
+	slice = String.prototype.slice,
+	gOPD = Object.getOwnPropertyDescriptor;
 
-var toStr = Object.prototype.toString;
-var hasToStringTag = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol';
-
-var typedArrays = {
-	Float32Array: true,
-	Float64Array: true,
-	Int8Array: true,
-	Int16Array: true,
-	Int32Array: true,
-	Uint8Array: true,
-	Uint8ClampedArray: true,
-	Uint16Array: true,
-	Uint32Array: true
-};
-
-var slice = String.prototype.slice;
-var toStrTags = {};
-var gOPD = Object.getOwnPropertyDescriptor;
-if (hasToStringTag && gOPD && Object.getPrototypeOf) {
-	forEach(typedArrays, function (_, typedArray) {
-		var arr = new global[typedArray]();
-		if (!(Symbol.toStringTag in arr)) {
-			throw new EvalError('this engine has support for Symbol.toStringTag, but ' + typedArray + ' does not have the property! Please report this.');
+module.exports = Object.getPrototypeOf && gOPD && typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol' ? function (value) {
+	var typedArray, arr, proto, descriptor;
+	/* eslint max-statements: ['error', 16] */
+	/* eslint no-restricted-syntax: ['warn', 'ForInStatement'] */
+	/* jscs:disable disallowNodeTypes */
+	for (typedArray in typedArrays) {
+		if (Object.prototype.hasOwnProperty.call(typedArrays, typedArray)) {
+			arr = new global[typedArray]();
+			if (!(Symbol.toStringTag in arr)) {
+				throw new EvalError('this engine has support for Symbol.toStringTag, but ' + typedArray + ' does not have the property! Please report this.');
+			}
+			proto = Object.getPrototypeOf(arr);
+			descriptor = gOPD(proto, Symbol.toStringTag);
+			if (!descriptor) {
+				proto = Object.getPrototypeOf(proto);
+				descriptor = gOPD(proto, Symbol.toStringTag);
+			}
+			if (descriptor) {
+				descriptor = typedArrays[descriptor.get.call(Object(value))];
+				if (descriptor) { return descriptor; }
+			}
 		}
-		var proto = Object.getPrototypeOf(arr);
-		var descriptor = gOPD(proto, Symbol.toStringTag);
-		if (!descriptor) {
-			var superProto = Object.getPrototypeOf(proto);
-			descriptor = gOPD(superProto, Symbol.toStringTag);
-		}
-		toStrTags[typedArray] = descriptor.get;
-	});
-}
-
-var tryTypedArrays = function tryTypedArrays(value) {
-	var anyTrue = false;
-	forEach(toStrTags, function (getter, typedArray) {
-		if (!anyTrue) {
-			try {
-				anyTrue = getter.call(value) === typedArray;
-			} catch (e) { /**/ }
-		}
-	});
-	return anyTrue;
-};
-
-module.exports = function isTypedArray(value) {
-	if (!value || typeof value !== 'object') { return false; }
-	if (!hasToStringTag) { return !!typedArrays[slice.call(toStr.call(value), 8, -1)]; }
-	if (!gOPD) { return false; }
-	return tryTypedArrays(value);
+	}
+	/* jscs:enable disallowNodeTypes */
+	return false;
+} : function (value) {
+	return typedArrays[slice.call(toStr.call(Object(value)), 8, -1)] || false;
 };
