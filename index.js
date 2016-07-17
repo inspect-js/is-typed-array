731fddd3feafa58b9b2ff0ba12ceeb7cce8428ea
xguest <xguest@list.ru>
2016-07-17 06:30:55 +0500

    replace the use of the forEach

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var getprototypeof = require('getprototypeof');
-
 var toStr = Object.prototype.toString;
 var hasToStringTag = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol';
 
@@ -19,26 +17,29 @@ var typedArrays = {
 
 var slice = String.prototype.slice;
 var gOPD = Object.getOwnPropertyDescriptor;
-
-module.exports = function isTypedArray(value) {
-	var out;
-	if (Object(value) === value) {
-		var proto, descriptor;
-		if (hasToStringTag && gOPD) {
-			if (Symbol.toStringTag in value) {
-				proto = getprototypeof(value);
-				descriptor = gOPD(proto, Symbol.toStringTag);
-				if (!descriptor) {
-					proto = getprototypeof(proto);
-					descriptor = gOPD(proto, Symbol.toStringTag);
-				}
-				if (descriptor && descriptor.get) {
-					out = descriptor.get.call(value);
-				}
+var tryTypedArrays = function isTypedArray(value) {
+	var typedArray;
+	if (Object.getPrototypeOf) {
+		if (Symbol.toStringTag in value) {
+			//
+			//
+			var proto = Object.getPrototypeOf(value);
+			var descriptor = gOPD(proto, Symbol.toStringTag);
+			if (!descriptor) {
+				var superProto = Object.getPrototypeOf(proto);
+				descriptor = gOPD(superProto, Symbol.toStringTag);
 			}
-		} else {
-			out = slice.call(toStr.call(value), 8, -1);
+			if (descriptor && descriptor.get) {
+				typedArray = descriptor.get.call(value);
+			}
 		}
 	}
-	return !!typedArrays[out];
+	return !!typedArrays[typedArray];
+};
+
+module.exports = function isTypedArray(value) {
+	if (!value || typeof value !== 'object') { return false; }
+	if (!hasToStringTag) { return !!typedArrays[slice.call(toStr.call(value), 8, -1)]; }
+	if (!gOPD) { return false; }
+	return tryTypedArrays(value);
 };
