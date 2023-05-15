9b4b09b6f90327a05a5a07dd94da183488d0e845
legobt <6wbvkn0j@anonaddy.me>
2023-05-15 02:57:30 +0000

    deps: remove call-bind

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 'use strict';
 
 var availableTypedArrays = require('available-typed-arrays');
-var callBound = require('call-bind/callBound');
 
-var $toString = callBound('Object.prototype.toString');
 var hasToStringTag = require('has-tostringtag/shams')();
 var gOPD = require('gopd');
 
 var g = typeof globalThis === 'undefined' ? global : globalThis;
 var typedArrays = availableTypedArrays();
 
-var $indexOf = callBound('Array.prototype.indexOf', true) || function indexOf(array, value) {
-	for (var i = 0; i < array.length; i += 1) {
-		if (array[i] === value) {
-			return i;
-		}
-	}
-	return -1;
-};
-var $slice = callBound('String.prototype.slice');
 var toStrTags = {};
 var getPrototypeOf = Object.getPrototypeOf; // require('getprototypeof');
 if (hasToStringTag && gOPD && getPrototypeOf) {
@@ -54,8 +43,8 @@ var tryTypedArrays = function tryAllTypedArrays(value) {
 module.exports = function isTypedArray(value) {
 	if (!value || typeof value !== 'object') { return false; }
 	if (!hasToStringTag || !(Symbol.toStringTag in value)) {
-		var tag = $slice($toString(value), 8, -1);
-		return $indexOf(typedArrays, tag) > -1;
+		var tag = String.prototype.slice.call(Object.prototype.toString.call(value), 8, -1);
+		return Array.prototype.indexOf.call(typedArrays, tag) > -1;
 	}
 	if (!gOPD) { return false; }
 	return tryTypedArrays(value);
